3aa48ee136c9eec080154954e6d2dffc8e13a649
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-04-17 19:31:58 -0400

    refactored code to make more sense to other people

--- 1-to-do-list-localStorage/script.js
+++ 1-to-do-list-localStorage/script.js
@@ -20,8 +20,6 @@ Also it helps to understand that you're creating a to-do list as well.
 
 //  Function to add a new task
 // Get the input field element using its ID "task"
-
-
 function addTask()
 {
     const input = document.getElementById("task");
@@ -55,7 +53,6 @@ function displayTasks()
 // 1. Get the <ul> element where we will show the list of tasks - name of variable "taskList"
 const taskList = document.getElementById("taskList");
 // 2. Clear any previous content inside the list so it doesn't repeat
-let resizeCounter=0;
 for(let i =taskList.children.length-1; i > -1; i--)
 {
     taskList.children[i].remove();
@@ -64,22 +61,22 @@ for(let i =taskList.children.length-1; i > -1; i--)
 let savedTasks = localStorage.getItem("tasks");
 let parsedTasks;
 savedTasks !== null ? parsedTasks = JSON.parse(savedTasks) : parsedTasks=[];
+
 // 4. Use forEach to loop through the array and display each task
-let count=0; 
+let count=0;                 
 parsedTasks.forEach((task)=>{
-    count++;
-    // 5. Create a new <li> element for each task
-    const newItem = document.createElement("li");
-    const deleteBtn= document.createElement("button");
+    count++;                                                             // iterate at the beginning of the loop, so that I can keep
+    // 5. Create a new <li> element for each task                       // my corresponding task indexes/locations aligned to their 
+    const newItem = document.createElement("li");                      // according location instead of + 1 ahead of its location or -1 
+    const deleteBtn= document.createElement("button");                // in its array(which is localStorage.getItem("tasks")).
 
     deleteBtn.textContent="remove current task";
-    deleteBtn.setAttribute("onclick",`removeTask(${count-1})`); // I need to link the functionality to remove with the associated list entry   
-                                                             // the reason this works because each list item, will have its own associated 
-                                                            // removeTask(1), removeTask(2), because I am creating literal html elements
-                                                           // with there own specific attributes
+    deleteBtn.setAttribute("onclick",`removeTask(${count-1})`);     // I need to link the functionality to remove with the associated list entry   
+                                                                   // the reason this works because each list item, will have its own associated 
+                                                                  // removeTask(1), removeTask(2), because I am creating literal html button elements
+                                                                 // with there own specific onclick() attributes
    
     // 6. Set the content of the <li> to show the task and a delete button - use template literal
-    //Might have to change this to textcontent but whatever
     newItem.textContent=`${count}. ${task}`;
     // 7. Add the <li> to the task list
     taskList.insertAdjacentElement("beforeend",newItem);
@@ -95,17 +92,21 @@ function removeTask(index)
 const currentTask= localStorage.getItem("tasks");
 let parsedTasks;
 currentTask !==null ? parsedTasks = JSON.parse(currentTask) : parsedTasks = [];
+
 // 2. Remove 1 task from the array at the given index - use splice method, turn string into an array though.
 parsedTasks.splice(index,1);
+
 // 3. Save the updated tasks back into LocalStorage
 localStorage.setItem("tasks",JSON.stringify(parsedTasks));
 // 4. Update the display so the removed task disappears from the screen
-// 5. Automatically run displayTasks() when the page loads
 displayTasks();
+
+// 5. Automatically run displayTasks() when the page loads
 window.addEventListener("load",(event)=>{
     displayTasks();
-})
-// So any saved tasks show up immediately
+});
 }
+//Regradless DisplayTask function gets called, when script.js is ran, 
+//because you have to display the saved tasked to the user.
 displayTasks();
 
